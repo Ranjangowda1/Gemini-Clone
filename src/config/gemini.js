7315a74b74f6c241e0ed5f16f908068f11a32642
Ranjan_17 <ranjangowdatn925@gmail.com>
2024-12-15 11:14:25 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,31 @@
+// const apikey="AIzaSyBcm2aQtgg5nLtRjzhOdY2H-TqTJ7cgCd4";
+import {GoogleGenerativeAI} from "@google/generative-ai";
+  
+
+  const genAI = new GoogleGenerativeAI("AIzaSyBcm2aQtgg5nLtRjzhOdY2H-TqTJ7cgCd4");
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+export default run;
